6b4035d0e683995eec05d4ad09f76886d7d1d065
Virgil <mariepauloine.santer@smail.com>
2021-10-20 17:09:30 +0200

    changement

--- cards/main.js
+++ cards/main.js
@@ -1,32 +1,54 @@
-const clone = (source) => {
-    const clone = source.cloneNode(true)
-    document.body.append (clone)
+const getRandomClassName = () => {
+  const r = Math.random()
 
-    const x = Math.round(Math.random() * window.innerWidth)
-    const y = Math.round(Math.random() * window.innerWidth)
-    clone.style.left = x + 'px'
-    clone.style.top = y + 'px'
-    const angle = Math.round(Math.random() * 60)
-    clone.style.transform = `rotate(${angle}deg)`
+  // 20% de chance de Silver
+  if (r < 0.2) {return 'silver'}
+
+  // 10% (30-20) de chance de gold
+  if (r < 0.3) {return 'gold'}
+
+  // dans tout les cas rien
+  return 'normal'
+}
+
+const getRandomEmoji = () =>{
+  return '🐉'
+}
+
+const clone = (source) => {
+  const clone = source.cloneNode(true)
+  document.body.append (clone)
+
+  const x = Math.round(Math.random() * window.innerWidth)
+  const y = Math.round(Math.random() * window.innerHeight)
+  clone.style.left = x + 'px'
+  clone.style.top = y + 'px'
+  const angle = Math.round(Math.random() * 360)
+  clone.style.transform = `rotate(${angle}deg)`
+
+  const randomClass = getRandomClassName ()
+  clone.className = `card ${randomClass}`
+
+  clone.querySelector('div').innerHTLM = getRandomEmoji()
 }
 
 const CloneOnClick = (event) => {
-    //console.log(event.currentTarget)
-    const source = event.currentTarget
-    clone (source)
+  //console.log(event.currentTarget)
+  const source = event.currentTarget
+  clone (source)
 }
 
 const destroyCard = (event) => {
-    //clone.log(envent.currentTarget)
-    const card = event.currentTarget.parentElement
-    //control.log(card)
-    card.remove()
-    event.stopPropagation()
+  //clone.log(envent.currentTarget)
+  const card = event.currentTarget.parentElement
+  //control.log(card)
+  card.remove()
+  event.stopPropagation()
 }
 
 window.onkeydown = (event) => {
-    //consol.log(event.code)
-    if (event.code === 'Space') {}
-    const source = document.querySelector('div.card')
-    clone (source)
+  //consol.log(event.code)
+  if (event.code === 'Space') {}
+  const source = document.querySelector('div.card')
+  clone (source)
 }
